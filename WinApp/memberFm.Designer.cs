ab16d2d19890cf1d06da253ecabd342f1ab6e6a1
ChiuJung <gchiujung@gmail.com>
2017-12-26 10:54:56 +0800

    init

---
+++ WinApp/memberFm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace WinApp
+{
+    partial class fm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dbvShowData = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dbvShowData)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(271, 22);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 0;
+            this.btnUpdate.Text = "更新";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(190, 22);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 1;
+            this.btnSearch.Text = "查詢";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dbvShowData
+            // 
+            this.dbvShowData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dbvShowData.Location = new System.Drawing.Point(-1, 51);
+            this.dbvShowData.Name = "dbvShowData";
+            this.dbvShowData.RowTemplate.Height = 24;
+            this.dbvShowData.Size = new System.Drawing.Size(357, 194);
+            this.dbvShowData.TabIndex = 2;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(12, 22);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(161, 22);
+            this.txtSearch.TabIndex = 3;
+            // 
+            // fm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(358, 245);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.dbvShowData);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnUpdate);
+            this.Name = "fm";
+            this.Text = "會員管理";
+            ((System.ComponentModel.ISupportInitialize)(this.dbvShowData)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dbvShowData;
+        private System.Windows.Forms.TextBox txtSearch;
+    }
+}
+
---
+++ WinApp/memberFm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Forms;
+using ActivitySystemEntity;
+
+
+namespace WinApp
+{
+    public partial class fm : Form
+    {
+        public fm()
+        {
+            InitializeComponent();
+        }
+        private BindingSource bindingSource = new BindingSource();
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {      
+            Member mm = new Member();
+            Member getMember = mm.selectSingleObject(txtSearch.Text);
+            bindingSource.Clear();
+            bindingSource.Add(getMember);
+            dbvShowData.AutoSize = true;
+            dbvShowData.DataSource = bindingSource;
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Member mm = new Member();
+            bool flag = mm.update();
+            if (flag)
+            {
+                MessageBox.Show("更新成功");
+            }
+            else {
+                MessageBox.Show("更新失敗");
+            };
+        }
+    }
+}
